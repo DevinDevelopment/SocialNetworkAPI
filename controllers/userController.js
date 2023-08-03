24656246861f9760ad1ead1335128eddc273c6d3
Devin Nunez <dnunez@gibbswire.com>
2023-08-02 23:22:25 -0400

    more user routes added

--- controllers/userController.js
+++ controllers/userController.js
@@ -35,7 +35,7 @@ module.exports = {
     }
   },
 
-  // Get a single student
+  // Get a single user
   async getSingleUser(req, res) {
     try {
       const user = await User.findOne({ _id: req.params.userId })
@@ -63,75 +63,39 @@ module.exports = {
       res.status(500).json(err);
     }
   },
-  // Delete a student and remove them from the course
+  // Delete a user and associated thoughts
   async deleteUser(req, res) {
     try {
-      const user = await User.findOneAndRemove({ _id: req.params.studentId });
+      const user = await User.findOneAndDelete({ _id: req.params.userId });
 
       if (!user) {
-        return res.status(404).json({ message: 'No user with this ID found.' });
+        return res.status(404).json({ message: 'No user with that ID' });
       }
 
-      const course = await Course.findOneAndUpdate(
-        { students: req.params.studentId },
-        { $pull: { students: req.params.studentId } },
-        { new: true }
+      await Thought.deleteMany({ _id: { $in: user.thought } });
+      res.json({ message: 'User and thoughts/reactions deleted!' })
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  // Update user
+  async updateUser(req, res) {
+    try {
+      const course = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body },
+        { runValidators: true, new: true }
       );
 
       if (!course) {
-        return res.status(404).json({
-          message: 'Student deleted, but no courses found',
-        });
+        res.status(404).json({ message: 'No course with this id!' });
       }
-
-      res.json({ message: 'Student successfully deleted' });
-    } catch (err) {
-      console.log(err);
+      res.json(course);
+    } 
+    catch (err) {
       res.status(500).json(err);
     }
   },
-
-  // Add an assignment to a student
-  async addAssignment(req, res) {
-    console.log('You are adding an assignment');
-    console.log(req.body);
-
-    try {
-      const student = await Student.findOneAndUpdate(
-        { _id: req.params.studentId },
-        { $addToSet: { assignments: req.body } },
-        { runValidators: true, new: true }
-      );
-
-      if (!student) {
-        return res
-          .status(404)
-          .json({ message: 'No student found with that ID :(' });
-      }
-
-      res.json(student);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  },
-  // Remove assignment from a student
-  async removeAssignment(req, res) {
-    try {
-      const student = await Student.findOneAndUpdate(
-        { _id: req.params.studentId },
-        { $pull: { assignment: { assignmentId: req.params.assignmentId } } },
-        { runValidators: true, new: true }
-      );
-
-      if (!student) {
-        return res
-          .status(404)
-          .json({ message: 'No student found with that ID :(' });
-      }
-
-      res.json(student);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  },
+  
 };
